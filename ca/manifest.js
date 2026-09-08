f14bf6b637077d2361bd12eece81f8fee816af0c
Nirvesh Verma <nirvesh777@gmail.com>
2026-09-08 13:26:51 +0530

    Delete ca directory

--- ca/manifest.js
+++
@@ -1,5 +0,0 @@
-// Registry of available months. Add one entry here (and one <script src="data/YYYY-MM.js">
-// in index.html) each time a new month's PDF is processed.
-window.MONTH_LIST = [
-  { key: "2026-08", label: "August 2026" }
-];
